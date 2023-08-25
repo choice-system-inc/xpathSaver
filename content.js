81af46b4cd80853ca3f1d6dcc2c5499415cd47bf
naoki-yamamoto <fuzimaru0421@gmail.com>
2023-08-26 08:15:59 +0900

    element の親要素が null である場合の修正

--- content.js
+++ content.js
@@ -2,15 +2,26 @@
 
 // マウスオーバーされた要素のXPathを取得する関数
 function getXPath(element) {
+  if (!element) {
+    return 'Element not found'; // エラー処理またはメッセージを返す
+  }
+
   if (element === document.body) {
     return 'body';
   }
 
-  const siblingElements = Array.from(element.parentNode.children);
+  const parentElement = element.parentNode;
+
+  if (!parentElement) {
+    return 'Parent element not found'; // エラー処理またはメッセージを返す
+  }
+
+  const siblingElements = Array.from(parentElement.children);
   const elementIndex = siblingElements.indexOf(element) + 1;
 
-  return `${getXPath(element.parentNode)}/${element.tagName.toLowerCase()}[${elementIndex}]`;
+  return `${getXPath(parentElement)}/${element.tagName.toLowerCase()}[${elementIndex}]`;
 }
+
 
 // メッセージを受け取り、XPathを送信
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
